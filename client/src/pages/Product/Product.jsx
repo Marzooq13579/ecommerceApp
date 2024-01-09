1cafca07e4af9d1a93052ec19f08e50631efa7af
Marzooq13579 <marzooqabs@gmail.com>
2024-01-09 23:28:03 +0530

    single product page design completed

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,10 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 import "./Product.scss";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(0);
 
-export default Product
+  const images = [
+    "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ];
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span>$199</span>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
+          possimus vitae ea soluta distinctio! Incidunt sunt quo quis magni,
+          architecto velit mollitia odit impedit! Quia repellendus consequuntur
+          deleniti exercitationem tempora architecto nostrum consectetur
+          voluptatem molestias omnis accusantium, adipisci reprehenderit quae
+          quas facilis dicta magnam dolore earum corporis maiores explicabo.
+          Perspiciatis!
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev == 0 ? 0 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+
+        <button className="add">
+          <AddShoppingCartIcon /> ADD TO CART
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISH LIST
+          </div>
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
