8c0ddc50faa25a10fbab096840783ebbba4b5625
Marzooq13579 <marzooqabs@gmail.com>
2024-01-12 14:44:25 +0530

    finished fetching single products

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./List.scss";
+import Card from "../Card/Card";
+import useFetch from "../../hooks/useFetch";
 
-const List = () => {
-  const data = [],
-    loading = true;
+const List = ({ subCats, maxPrice, sort, catId }) => {
+  
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+  );
+
 
   return (
     <div className="list">
--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -2,74 +2,97 @@ import React, { useState } from "react";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 import "./Product.scss";
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(0);
 
-  const images = [
-    "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  ];
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
+
   return (
     <div className="product">
-      <div className="left">
-        <div className="images">
-          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
-        </div>
-        <div className="mainImg">
-          <img src={images[selectedImg]} alt="" />
-        </div>
-      </div>
-      <div className="right">
-        <h1>Title</h1>
-        <span>$199</span>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-          possimus vitae ea soluta distinctio! Incidunt sunt quo quis magni,
-          architecto velit mollitia odit impedit! Quia repellendus consequuntur
-          deleniti exercitationem tempora architecto nostrum consectetur
-          voluptatem molestias omnis accusantium, adipisci reprehenderit quae
-          quas facilis dicta magnam dolore earum corporis maiores explicabo.
-          Perspiciatis!
-        </p>
-        <div className="quantity">
-          <button
-            onClick={() => setQuantity((prev) => (prev == 0 ? 0 : prev - 1))}
-          >
-            -
-          </button>
-          {quantity}
-          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-        </div>
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              <img
+                src={
+                  import.meta.env.VITE_API_UPLOAD_URL +
+                  data?.attributes?.img?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img")}
+              />
+              <img
+                src={
+                  import.meta.env.VITE_API_UPLOAD_URL +
+                  data?.attributes?.img2?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img2")}
+              />
+            </div>
+            <div className="mainImg">
+              <img
+                src={
+                  import.meta.env.VITE_API_UPLOAD_URL +
+                  data?.attributes[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="right">
+            <h1>{data?.attributes?.title}</h1>
+            <span>{data?.attributes?.price}</span>
+            <p>
+              {data?.attributes?.desc}
+            </p>
+            <div className="quantity">
+              <button
+                onClick={() =>
+                  setQuantity((prev) => (prev == 0 ? 0 : prev - 1))
+                }
+              >
+                -
+              </button>
+              {quantity}
+              <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+            </div>
 
-        <button className="add">
-          <AddShoppingCartIcon /> ADD TO CART
-        </button>
-        <div className="links">
-          <div className="item">
-            <FavoriteBorderIcon /> ADD TO WISH LIST
+            <button className="add">
+              <AddShoppingCartIcon /> ADD TO CART
+            </button>
+            <div className="links">
+              <div className="item">
+                <FavoriteBorderIcon /> ADD TO WISH LIST
+              </div>
+              <div className="item">
+                <BalanceIcon /> ADD TO COMPARE
+              </div>
+            </div>
+            <div className="info">
+              <span>Vendor: Polo</span>
+              <span>Product Type: T-Shirt</span>
+              <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="info">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
           </div>
-          <div className="item">
-            <BalanceIcon /> ADD TO COMPARE
-          </div>
-        </div>
-        <div className="info">
-          <span>Vendor: Polo</span>
-          <span>Product Type: T-Shirt</span>
-          <span>Tag: T-Shirt, Women, Top</span>
-        </div>
-        <hr />
-        <div className="info">
-          <span>DESCRIPTION</span>
-          <hr />
-          <span>ADDITIONAL INFORMATION</span>
-          <hr />
-          <span>FAQ</span>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
